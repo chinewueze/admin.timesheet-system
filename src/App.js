5e237d67b1c95c0920ebbec0dccb3e99bab83282
chinewueze <chiderauba83@gmail.com>
2023-11-04 20:19:49 +0100

    Added dynamic routing

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Route, Routes} from 'react-router-dom';
 import { SignIn } from "./components/Login";
 import {SpecificReportView } from "./components/ViewSpecificReport"
-import {  AdminTimesheetView } from "./components/AdminViewReport";
+import {  AdminViewAll } from "./components/AdminViewReport";
 
 export default function App() {
   
   return (
     <Routes>
       <Route path="/" element={<SignIn/>}/> 
-      <Route path="/view-reports" element={< AdminTimesheetView />}/> 
-      <Route path="/view-specific-report" element={<SpecificReportView />}/>
+      <Route path="/view-reports" element={< AdminViewAll />}/> 
+      <Route path="/view-specific-report/:userId" element={<SpecificReportView />}/>
       
     </Routes>
   );
